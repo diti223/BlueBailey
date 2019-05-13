e2fe1db8bff6b139206613d8f3377c9da15bf02f
Adrian Bilescu <adrian.bilescu@endava.com>
2019-05-13 16:31:05 +0300

    Drag & drop functionality in progress

--- BlueBailey/AppDelegate.swift
+++ BlueBailey/AppDelegate.swift
@@ -17,23 +17,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
-        let path = Path("Project/Bait.xcodeproj") // Your project path
-        guard let xcodeproj = try? XcodeProj(path: path) else {
-                return
-        }
-        let pbxproj = xcodeproj.pbxproj
-        // Returns a PBXProj
-        pbxproj.nativeTargets.forEach { target in
-            print(target.name)
-        }
-        let project = pbxproj.projects.first! // Returns a PBXProject
-        let mainGroup = project.mainGroup
-        try? mainGroup?.addGroup(named: "MyGroup")
-        try? xcodeproj.write(path: path)
-        var groupPath = Path("Project/MyGroup")
-        try? groupPath.mkdir()
-        groupPath = groupPath + Path("SubGroup")
-        try? groupPath.mkdir()
+//        let projectPath = Path("Project")
+//        if !projectPath.exists {
+//            try? Path("Project").mkdir()
+//        }
+//        let path = Path("Project/Bait/Bait.xcodeproj") // Your project path
+//        if !path.exists {
+//            
+//            let data = XCWorkspaceData(children: [])
+//            let workspace = XCWorkspace(data: data)
+//            let project = PBXProject(name: "Bait", buildConfigurationList: XCConfigurationList.init(buildConfigurations: [], defaultConfigurationName: "Debug", defaultConfigurationIsVisible: true), compatibilityVersion: "1.0", mainGroup: PBXGroup(children: [], sourceTree: nil, name: "Main", path: nil, includeInIndex: nil, wrapsLines: nil, usesTabs: nil, indentWidth: nil, tabWidth: nil))
+//            let proj = PBXProj(rootObject: project, objectVersion: 1, archiveVersion: 1, classes: [:], objects: [])
+//            let xcodeProj = XcodeProj(workspace: workspace, pbxproj: proj)
+//            try? xcodeProj.write(path: path)
+//        }
+//        
+//        guard let xcodeproj = try? XcodeProj(path: path) else {
+//            return
+//        }
+//        let pbxproj = xcodeproj.pbxproj
+//        // Returns a PBXProj
+//        pbxproj.nativeTargets.forEach { target in
+//            print(target.name)
+//        }
+//        let project = pbxproj.projects.first! // Returns a PBXProject
+//        let mainGroup = project.mainGroup
+//        try? mainGroup?.addGroup(named: "MyGroup")
+//        try? xcodeproj.write(path: path)
+//        var groupPath = Path("Project/MyGroup")
+//        try? groupPath.mkdir()
+//        groupPath = groupPath + Path("SubGroup")
+//        try? groupPath.mkdir()
         
     }
 
--- BlueBailey/ViewController.swift
+++ BlueBailey/ViewController.swift
@@ -9,10 +9,39 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var dropView: DropView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        dropView.wantsLayer = true
+        dropView.layer?.borderColor = CGColor.black.copy(alpha: 0.5)
+        dropView.layer?.borderWidth = 1
+        dropView.layer?.backgroundColor = NSColor.darkGray.cgColor
+        dropView.registerForDraggedTypes([NSPasteboard.PasteboardType.fileURL])
     }
     
+}
+
+class DropView: NSView {
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        debugPrint(#function)
+        return super.draggingEntered(sender)
+    }
+    
+//    override func draggingUpdated(_ sender: NSDraggingInfo) -> NSDragOperation {
+//        debugPrint(#function)
+//        return super.draggingUpdated(sender)
+//    }
+//
+    
+    override func draggingEnded(_ sender: NSDraggingInfo) {
+        debugPrint(#function)
+        guard let filePath = sender.draggingPasteboard.propertyList(forType: .fileURL) as? String else { return }
+        guard let url = URL(string: filePath) else { return }
+        let contents = try? FileManager.default.contents(atPath: url.path)
+        guard let content = try? Data(contentsOf: url) else { return }
+        guard let string = String(data: content, encoding: .utf8) else { return }
+        
+        super.draggingEnded(sender)
+    }
 }
